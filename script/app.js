fb431a4f920a62a3dbfd8b804277b8a18373f660
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-12-27 16:36:51 +0100

    Fixed more to dark/daymode

--- script/app.js
+++ script/app.js
@@ -8,6 +8,10 @@ function main() {
 let mainBg = document.querySelector('main')   
 let asideBg = document.querySelector('aside')   
 const switchMode = document.querySelector('#mode') 
+const changeLogo = document.querySelector('.logo')
+
+console.log(changeLogo)
+
 
 
 function addEventListeners() {
@@ -21,10 +25,12 @@ function switchModes() {
     if(!mainBg.classList.contains("dark-mode")) {
         mainBg.classList.add("dark-mode")
         asideBg.classList.add("dark-mode-aside")
+        changeLogo.src = "./style/img/logo-daymode.png";
         console.log(mainBg)
     } else {
         mainBg.classList.remove("dark-mode");
         asideBg.classList.remove("dark-mode-aside")
+        changeLogo.src = "./style/img/logo-darkmode.png";
     }
 }
 
